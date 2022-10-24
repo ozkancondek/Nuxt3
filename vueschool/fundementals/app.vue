857961e0e0ad5bb674d9e1c7154d1e13a395adf3
ozkancondek <ozkncndek@gmail.com>
2022-10-24 11:36:04 +0200

    Basic routing with NuxtLink

--- vueschool/fundementals/app.vue
+++ vueschool/fundementals/app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <h1>Hello from Nuxtx Fundementals</h1>
+    <nav style="display: flex; gap: 20px">
+      <!--     <a href="/">Home</a>
+      <a href="movies">Movies</a> -->
+      <!--  I dont wanna refresh all page when i route to another page -->
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="movies">Movies</NuxtLink>
+      <NuxtLink to="https://v3.nuxtjs.org/" target="_blank"
+        >Nuxt3 Website</NuxtLink
+      >
+    </nav>
+    <NuxtPage></NuxtPage>
   </div>
 </template>
