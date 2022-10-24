52b53755ad5242a379a977b7a910d53fb25a45d2
ozkancondek <ozkncndek@gmail.com>
2022-10-24 14:12:15 +0200

    add pick to useAsyncData

--- vueschool/fundementals/pages/movies/[id].vue
+++ vueschool/fundementals/pages/movies/[id].vue
@@ -7,7 +7,27 @@
 <script setup>
 const route = useRoute();
 //i will give unique parameter for cache
-const { data } = useAsyncData(`/movies/${route.params.id}`, () => {
-  return $fetch(`http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`);
-});
+const { data } = useAsyncData(
+  `/movies/${route.params.id}`,
+  () => {
+    return $fetch(
+      `http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`
+    );
+  },
+  {
+    //I can choose the data easly with pick or override it with transform
+    pick: ["Plot", "Title"], //BP
+
+    /*     transform(data) {
+      //if I just wanna object title
+     // return data.Title;
+     //And I can also return an object
+     return {
+      Plot:data.Plotm,
+      Title : data.Title
+     }
+    }, */
+  }
+);
+console.log(data);
 </script>
