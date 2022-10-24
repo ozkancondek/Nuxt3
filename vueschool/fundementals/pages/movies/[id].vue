201be12beac9be4c85763ccc461d4d01b9bb0a0a
ozkancondek <ozkncndek@gmail.com>
2022-10-24 13:54:08 +0200

    useAsyncData key

--- vueschool/fundementals/pages/movies/[id].vue
+++ vueschool/fundementals/pages/movies/[id].vue
@@ -6,8 +6,8 @@
 
 <script setup>
 const route = useRoute();
-
-const { data } = useAsyncData(() => {
+//i will give unique parameter for cache
+const { data } = useAsyncData(`/movies/${route.params.id}`, () => {
   return $fetch(`http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`);
 });
 </script>
