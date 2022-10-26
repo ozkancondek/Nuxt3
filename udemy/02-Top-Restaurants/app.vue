7113b5093de3baa5a2481d4c8fc41e8894598cc8
ozkancondek <ozkncndek@gmail.com>
2022-10-26 10:52:42 +0200

    15-Navbar implemented

--- udemy/02-Top-Restaurants/app.vue
+++ udemy/02-Top-Restaurants/app.vue
@@ -1,3 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <Nav />
+    <div class="container">
+      <h1>Welcome to Restaurantly</h1>
+
+      <hr />
+      <a href="/restaurants">Go to Restaurants</a>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
