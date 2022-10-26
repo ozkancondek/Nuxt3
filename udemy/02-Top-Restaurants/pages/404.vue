60ed74d6af318763c1aefe012679fe6a59b2c3da
ozkancondek <ozkncndek@gmail.com>
2022-10-26 11:59:55 +0200

    18-error page added

---
+++ udemy/02-Top-Restaurants/pages/404.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Nav />
+    <div class="container">
+      <h1>Page Not Found</h1>
+
+      <hr />
+      <a href="/restaurants">Go back</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
