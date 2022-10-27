bde55ca814c6f71c06b56811b49b0e62a69e8951
ozkancondek <ozkncndek@gmail.com>
2022-10-27 17:10:47 +0200

    32-add composables and collect methods

--- udemy/TodoApp/app.vue
+++ udemy/TodoApp/app.vue
@@ -1,9 +1,57 @@
-<script setup>
-const { data } = useFetch("http://localhost:3000/api/todo");
+<script setup lang="ts">
+const input = ref("");
+const { todos, addTodo, updateTodo, deleteTodo } = useTodos();
+const handleClick = () => {
+  addTodo(input.value);
+  input.value = "";
+};
 </script>
 
 <template>
-  <div>
-    {{ data }}
+  <div class="container">
+    <NCard class="cards">
+      <h1>My Todos</h1>
+      <div class="add-todo">
+        <input placeholder="Add a new todo..." v-model="input" />
+        <NButton @click="handleClick">Add</NButton>
+      </div>
+      <NCard
+        @click="() => updateTodo(todo.id)"
+        class="card"
+        v-for="todo in todos"
+        :key="todo.id"
+      >
+        <h4 :class="todo.completed ? 'complete' : null">{{ todo.item }}</h4>
+        <p @click="() => deleteTodo(todo.id)">❌</p>
+      </NCard>
+    </NCard>
   </div>
 </template>
+
+<style scoped>
+.container {
+  padding: 2rem;
+  margin: 0 auto;
+  max-width: 50%;
+}
+.cards {
+  padding: 2rem;
+}
+input {
+  outline: none;
+}
+.add-todo {
+  display: flex;
+  justify-content: space-between;
+}
+.card {
+  padding: 0.5rem;
+  margin-top: 1rem;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+}
+.complete {
+  text-decoration: line-through;
+}
+</style>
