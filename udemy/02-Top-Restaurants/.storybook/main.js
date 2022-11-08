6f9b7daf1e670d9e8a08cc3c15e01f966cd2ddd1
ozkancondek <ozkncndek@gmail.com>
2022-11-08 13:36:25 +0100

    initial commit

---
+++ udemy/02-Top-Restaurants/.storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: ["@storybook/addon-essentials"],
+  core: {
+    builder: "storybook-builder-vite",
+  },
+};
