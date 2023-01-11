d4a843a7f1d045e9c306cd354880df802db68a75
ozkancondek <ozkncndek@gmail.com>
2023-01-11 20:44:30 +0100

    add Nuxtlink

--- masterin/masterinProject/pages/course.vue
+++ masterin/masterinProject/pages/course.vue
@@ -8,8 +8,27 @@
       if i go to the further route, i will see that i have elements of /course
       root
     </p>
+    <hr />
+    <br />
+    <br />
+    <h1><strong>Chapters</strong></h1>
+    <div v-for="chapter in chapters" :key="chapter.slug">
+      <h4>{{ chapter.title }}</h4>
+      <hr />
+
+      <NuxtLink
+        v-for="(lesson, index) in chapter.lessons"
+        :key="lesson.slug"
+        :to="`/course/chapter/${chapter.slug}/lesson/${lesson.slug}`"
+      >
+        <span>{{ index + 1 }}.</span> <span>{{ lesson.title }}</span> <br />
+      </NuxtLink>
+    </div>
+
     <NuxtPage />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const { chapters } = useCourse();
+</script>
