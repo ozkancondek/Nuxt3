dcfb51d7bdb1760403f51ed6631912f7331b4607
ozkancondek <ozkncndek@gmail.com>
2023-01-18 17:24:30 +0100

    adding ts

--- masterin/masterinProject/composables/useCourse.ts
+++ masterin/masterinProject/composables/useCourse.ts
@@ -1,4 +1,26 @@
 import courseData from "./courseData"
+
+type Lesson = {
+  title:string;
+  slug:string;
+  number:number;
+  downloadUrl:string;
+  videoId:number;
+  text:string;
+  sourceUrl?:string;
+}
+export type Chapter = {
+  title: string;
+  slug: string;
+  number: number;
+  lessons: Lesson[] ;
+};
+
+export type Course = {
+  title: string;
+  chapters: Chapter[];
+};
+
 export const useCourse = () => {
   return courseData
 }
