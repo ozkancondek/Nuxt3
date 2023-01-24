608f3a78623db539346706ac6e9751ef11e3b90f
ozkancondek <ozkncndek@gmail.com>
2023-01-24 18:12:17 +0100

    define page meta

--- masterin/masterinProject/pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ masterin/masterinProject/pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -83,6 +83,31 @@ const toggleComplete = () => {
 useHead({
   title: title.value,
 });
+definePageMeta({
+  middleware: function ({ params }, from) {
+    const course = useCourse();
+    const chapter = course.chapters.find(
+      (chapter) => chapter.slug === params.chapterSlug
+    );
+    if (!chapter) {
+      return abortNavigation(
+        createError({
+          statusCode: 404,
+          message: "Chapter not found",
+        })
+      );
+    }
+    const lesson = chapter.lessons.find(
+      (lesson) => lesson.slug === params.lessonSlug
+    );
+    if (!lesson) {
+      return abortNavigation(
+        createError({
+          statusCode: 404,
+          message: "lesson not found",
+        })
+      );
+    }
+  },
+});
 </script>
-
-<!-- <ClientOnly></ClientOnly>  tag for .clent-->
