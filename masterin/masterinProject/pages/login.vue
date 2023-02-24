72c32766681b28b31e405e6667c2d14a793ed595
ozkancondek <oezkan.coendek@obi.de>
2023-02-24 17:06:37 +0100

    authentication

--- masterin/masterinProject/pages/login.vue
+++ masterin/masterinProject/pages/login.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="prose w-full max-w-2xl h-9">
-    <h1>Login to reach whole chapters</h1>
-
-    <br />
-
+    <h1>Log in to {{ course.title }}</h1>
     <button
       class="bg-blue-500 text-white font-bold py-2 px-4 rounded"
       @click="login"
@@ -14,14 +11,25 @@
 </template>
 
 <script setup lang="ts">
-const { title } = useCourse();
+const course = useCourse();
+const { query } = useRoute();
 const supabase = useSupabaseClient();
+const user = useSupabaseUser();
+watchEffect(async () => {
+  if (user.value) {
+    await navigateTo(query.redirectTo as string, {
+      replace: true,
+    });
+  }
+});
 const login = async () => {
+  const redirectTo = `${window.location.origin}${query.redirectTo}`;
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "github",
+    options: { redirectTo },
   });
   if (error) {
-    console.log(error);
+    console.error(error);
   }
 };
 </script>
